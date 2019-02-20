c45cc7f062a32fdb9a5508b65af19b74382c4bd4
EdwinSaucedo <edwin.saucedo.upgop@gmail.com>
2019-02-20 09:53:51 -0600

    Add practice

---
+++ edwinSaucedo1402StoryBoard/edwin3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  edwin3
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 20/02/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
